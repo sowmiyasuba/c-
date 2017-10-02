97ecf77192cb53bcee415b4cbb1976bac6ece9e2
sowmiyasuba <31585889+sowmiyasuba@users.noreply.github.com>
2017-10-02 22:36:15 +0530

    Create largest among three num.c

---
+++ largest among three num.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+int a,b,c;
+printf("Enter the numbers");
+scanf("%d%d%d",&a&b&c);
+if(a>=b&&a>=c)
+{
+printf(A is grater");
+}
+if(b>a&&b>c)
+{
+printf("B is greater");
+}
+if(c>=a&&c>=b)
+printf("C is greater");
+}
+return o;
+}
